5b1553de1e42420cdaedcb2a76432d9d1a55abe4
lnoemeyer <lukas.noemeyer@gmail.com>
2024-03-25 12:51:33 +0100

    fix: added correct Drools Task loading

--- src/app/task-type/task-type-drools/task-type-drools.component.ts
+++ src/app/task-type/task-type-drools/task-type-drools.component.ts
@@ -46,6 +46,11 @@ export class TaskTypeDroolsComponent extends TaskTypeFormComponent<TaskTypeForm>
     super();
   }
 
+  override set formData(data: unknown) {
+    super.formData = data;
+    this.setDroolsData(data);
+  }
+
   protected override initForm(): void {
     this.form.addControl('solution', new FormControl<string | null>(null));
     this.form.addControl('objects', new FormControl<string | null>(null));
@@ -69,11 +74,28 @@ export class TaskTypeDroolsComponent extends TaskTypeFormComponent<TaskTypeForm>
   removeDroolsClass(index: number) {
     this.classes.removeAt(index);
   }
+
+  setDroolsData(dto: any) {
+    // Clear the existing form array
+    while (this.classes.length !== 0) {
+      this.classes.removeAt(0);
+    }
+
+    // Add the classes from the DTO to the form array
+    dto.classes.forEach((c: any) => {
+      const classForm = new FormGroup({
+        classname: new FormControl(c.classname),
+        classBody: new FormControl(c.classBody)
+      });
+      this.classes.push(classForm);
+    });
+  }
 }
+
 
 interface TaskTypeForm {
   solution: FormControl<string | null>;
-  classes: FormArray<FormGroup<{ classname: FormControl<string | null>, classBody: FormControl<string | null> }>>;
+  classes: FormArray<FormGroup<{ classname: FormControl<string | null>, classBody: FormControl<string | null>}>>;
   objects: FormControl<string | null>;
   validationClassname: FormControl<string | null>;
   errorWeighting: FormControl<number | null>;
