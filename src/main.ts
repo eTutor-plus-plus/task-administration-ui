dd81f22b025f91f3a39896dd694423c0b78017ad
Martin <martin@straszer.eu>
2024-06-19 11:32:09 +0200

    feat: Added custom WYSIWYG editor
    Closes: #36

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import '@angular/common/locales/global/de-AT';
 
-import Quill from 'quill';
-import htmlEditButton from 'quill-html-edit-button';
-
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-
-Quill.register({
-  'modules/htmlEditButton': htmlEditButton
-});
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
