9b7a5602bd42cd91c79dd5b18d4698d78f56a6b9
NoahStallinger <noahstalli11@gmail.com>
2026-04-13 16:37:49 +0200

    feat(insertStatement): added logic for adding InsertStatements per Constraint, added Whitelist field, removed points per column

--- src/app/task-type/task-type-sql-ddl/task-type-sql-ddl.component.ts
+++ src/app/task-type/task-type-sql-ddl/task-type-sql-ddl.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { MonacoEditorModule } from 'ngx-monaco-editor-v2';
 import { PaginatorModule } from 'primeng/paginator';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { TranslocoDirective, TranslocoPipe } from '@ngneat/transloco';
 import { ButtonModule } from 'primeng/button';
 import { TreeSelectModule } from 'primeng/treeselect';
 import { TaskTypeFormComponent } from '../task-type-form.component';
 import { editor } from 'monaco-editor';
 import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
 import { NgForOf } from '@angular/common';
 
 @Component({
@@ -21,6 +22,7 @@ import { NgForOf } from '@angular/common';
     TranslocoPipe,
     TreeSelectModule,
     InputTextModule,
+    InputTextareaModule,
     NgForOf,
     ReactiveFormsModule
   ],
@@ -33,6 +35,7 @@ export class TaskTypeSqlDdlComponent extends TaskTypeFormComponent<TaskTypeForm>
   readonly editorOptions: editor.IStandaloneEditorConstructionOptions = {
     language: 'sql'
   };
+  insertStatements = new FormArray<FormGroup<{ name: FormControl<string | null>, insertStatements: FormControl<string | null> }>>([]);
 
   constructor() {
     super();
@@ -40,27 +43,63 @@ export class TaskTypeSqlDdlComponent extends TaskTypeFormComponent<TaskTypeForm>
 
   override set formData(data: unknown) {
     super.formData = data;
+    this.setSqlDdlData(data);
   }
 
   protected override initForm(): void {
     this.form.addControl('solution', new FormControl<string | null>(null));
-    this.form.addControl('insertStatements', new FormControl<string | null>(null));
+    this.form.addControl('whitelist', new FormControl<string | null>(null));
+    this.form.addControl('insertStatements', this.insertStatements);
     this.form.addControl('tablePoints', new FormControl<number | null>(null));
-    this.form.addControl('columnPoints', new FormControl<number | null>(null));
     this.form.addControl('primaryKeyPoints', new FormControl<number | null>(null));
     this.form.addControl('foreignKeyPoints', new FormControl<number | null>(null));
     this.form.addControl('constraintPoints', new FormControl<number | null>(null));
   }
 
+  addInsertStatement() {
+    this.insertStatements.push(new FormGroup({
+      name: new FormControl<string | null>(''),
+      insertStatements: new FormControl<string | null>('')
+    }));
+  }
+
+  removeInsertStatement(index: number) {
+    this.insertStatements.removeAt(index);
+  }
+
+  private setSqlDdlData(dto: any) {
+    while (this.insertStatements.length !== 0) {
+      this.insertStatements.removeAt(0);
+    }
+
+    const insertStatements = dto?.insertStatements;
+    if (Array.isArray(insertStatements)) {
+      insertStatements.forEach((entry: any) => {
+        this.insertStatements.push(new FormGroup({
+          name: new FormControl<string | null>(entry?.name ?? ''),
+          insertStatements: new FormControl<string | null>(entry?.insertStatements ?? '')
+        }));
+      });
+      return;
+    }
+
+    //relevant if you want to load old tasks where insertStatements was a string not an array
+    if (typeof insertStatements === 'string') {
+      this.insertStatements.push(new FormGroup({
+        name: new FormControl<string | null>(''),
+        insertStatements: new FormControl<string | null>(insertStatements)
+      }));
+    }
+  }
 
 }
 
 
 interface TaskTypeForm {
   solution: FormControl<string | null>;
-  insertStatements: FormControl<string | null>;
+  whitelist: FormControl<string | null>;
+  insertStatements: FormArray<FormGroup<{ name: FormControl<string | null>, insertStatements: FormControl<string | null> }>>;
   tablePoints: FormControl<number | null>;
-  columnPoints: FormControl<number | null>;
   primaryKeyPoints: FormControl<number | null>;
   foreignKeyPoints: FormControl<number | null>;
   constraintPoints: FormControl<number | null>;
